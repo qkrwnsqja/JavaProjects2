b7f5cc9d770313e48273fe72c41c72e86d7418fb
qkrwnsqja <wnsgks0205@gmail.com>
2025-09-24 11:46:56 +0900

    chapter11. Exception2 chapter 12.GUI

--- src/w0924/ArrayExceptionTest.java
+++ src/w0924/ArrayExceptionTest.java
@@ -10,7 +10,7 @@ public class ArrayExceptionTest {
             arr[3] = 40;
 
             for (int i = 0; i < arr.length; i++) {
-                System.out.println(arr[i] + '\t');
+                System.out.print(arr[i] + '\t');
             }
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("배열의 인덱스 번호가 범위를 벗어난 것 같습니다.");
