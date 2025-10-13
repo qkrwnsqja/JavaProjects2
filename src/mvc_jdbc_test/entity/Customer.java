51ca3f745ca30394793a6f79bd54a03d95406eb0
qkrwnsqja <wnsgks0205@gmail.com>
2025-10-13 17:13:30 +0900

    DB + Java => MVC ing

---
+++ src/mvc_jdbc_test/entity/Customer.java
@@ -0,0 +1,58 @@
+package mvc_jdbc_test.entity;
+
+public class Customer {
+    private String customerid;
+    private String customername;
+    private int age;
+    private String level;
+    private String job;
+    private int reward; //적립금
+
+    public String getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(String customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getCustomername() {
+        return customername;
+    }
+
+    public void setCustomername(String customername) {
+        this.customername = customername;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+}
